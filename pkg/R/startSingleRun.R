41ab143e931a87f24d50625bec436ac1b93c4765
Brian O'Meara <bomeara@users.noreply.github.com>
2011-12-05 07:27:11 +0000

    better path

--- pkg/R/startSingleRun.R
+++ pkg/R/startSingleRun.R
@@ -1,5 +1,5 @@
 setwd("/Users/bomeara/Dropbox/CarstensScratch/analysis")
-source("~/Documents/MyDocuments/Active/phrapl/src/R/batchMS.R")
+source("~/Documents/MyDocuments/Active/phrapl/pkg/R/batchMS.R")
 itnmax=40
 popVector<-c(3,4,4)
 maxK<-2
