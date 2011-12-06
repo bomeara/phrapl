a67fdfe49d71af81e38a44acf4b8e17fd38aa01d
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-06 15:53:50 +0000

    better ms location

--- pkg/R/individualRunTestPerformance.R
+++ pkg/R/individualRunTestPerformance.R
@@ -1,4 +1,4 @@
-individualRunTestPerformance<-function(filename="testRunResult.Rsave",batchMSlocation="../batchMS.R",compareLocation="../comparecladespipe.pl",msLocation="/home/bomeara/bin/ms",popVector=c(3,4,4),maxK=2,itnmax=40,nTrees=1000000,migrationArray=NULL,migrationArrayMap=NULL, trueModelID=5,trueModelParams=c(0.9,0.2),nTreesObserved=3) {
+individualRunTestPerformance<-function(filename="testRunResult.Rsave",batchMSlocation="../batchMS.R",compareLocation="../comparecladespipe.pl",msLocation="/usr/local/bin/ms",popVector=c(3,4,4),maxK=2,itnmax=40,nTrees=1000000,migrationArray=NULL,migrationArrayMap=NULL, trueModelID=5,trueModelParams=c(0.9,0.2),nTreesObserved=3) {
   source(batchMSlocation)
   if(is.null(migrationArray)) {
     migrationArray<-generateMigrationIndividualsAllowNoMigration(popVector,maxK=maxK)
