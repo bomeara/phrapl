9ad8a40b6669da8650b903d52ac0146fb7725253
Brian O'Meara <bomeara@users.noreply.github.com>
2011-12-06 05:36:13 +0000

    getting ready for group runs

--- pkg/R/individualRunTestPerformance.R
+++ pkg/R/individualRunTestPerformance.R
@@ -1,3 +1,17 @@
-individualRunTestPerformance<-function(popVector,maxK,itnmax=40,nTrees=1000000,migrationArray,migrationArrayMap, trueModelID,nTreesObserved) {
-  
+individualRunTestPerformance<-function(batchMSlocation="../batchMS.R",compareLocation="../comparecladespipe.pl",msLocation="/home/bomeara/bin/ms",popVector=c(3,4,4),maxK=2,itnmax=40,nTrees=1000000,migrationArray=NULL,migrationArrayMap=NULL, trueModelID=5,trueModelParams=c(0.9,0.2),nTreesObserved=3) {
+  source(batchMSlocation)
+  if(is.null(migrationArray)) {
+    migrationArray<-generateMigrationIndividualsAllowNoMigration(popVector,maxK=maxK)
+  }
+  if(is.null(migrationArrayMap)) {
+    migrationArrayMap<-generateMigrationArrayMap(migrationArray)
+  }
+  createAssignment(popVector)
+  trueModel<-migrationArray[[trueModelID]]
+  names(trueModelParams)<-msIndividualParameters(trueModel)
+  msCallInfo<-createMSstringSpecific(popVector,trueModel,trueModelParams,nTrees=3)
+  system(paste(msLocation=msLocation,sprintf("%i",msCallInfo$nsam),sprintf("%i",msCallInfo$nreps),msCallInfo$opts," | grep ';' > observed.tre"),intern=FALSE)
+  result<-searchDiscreteModelSpaceOptim(migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, nTrees=10000 ,msLocation=msLocation,compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.tre",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=TRUE, debug=FALSE,itnmax=itnmax,method="BFGS",print.level=2)
+  print(result)
+  save(result,file="testRunResult.Rsave",compress=TRUE)
 }
