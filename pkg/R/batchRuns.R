9e482642bd040a7efffac132a9766c14b746dc90
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-07 15:36:54 +0000

    better batch files and different optimizer

--- pkg/R/batchRuns.R
+++ pkg/R/batchRuns.R
@@ -1,22 +1,24 @@
 library(parallel)
 itnmaxVector<-c(2,25,100)
 nTreesVector<-c(10000,1000000,100000000)
-nrep<-6
-doSingleRun<-function(itnmax,nTrees,rep) {
-  newDir<-paste("itnmax",itnmax,"_nTrees",nTrees,"_rep",rep,sep="")
+nTreesObservedVector<-c(3,10,100)
+nrep<-10
+doSingleRun<-function(itnmax,nTrees,nTreesObserved,rep) {
+  newDir<-paste("itnmax",itnmax,"_nTrees",nTrees,"_nTreesObs",nTreesObserved,"_rep",rep,sep="")
   filename<-paste(newDir,".Rsave",sep="")
   system(paste("mkdir ",newDir))
   setwd(newDir)
   print(system("pwd"))
   system('cp ../*.pl .')
   source("../individualRunTestPerformance.R")
-  individualRunTestPerformance(filename=filename,itnmax=itnmax, nTrees=nTrees)
+  individualRunTestPerformance(filename=filename,itnmax=itnmax, nTrees=nTrees, nTreesObserved=nTreesObserved)
 }
-singleParamDoSingleRun<-function(itnmax_nTrees_rep) {
+singleParamDoSingleRun<-function(itnmax_nTrees_nTreesObserved_rep) {
   x<-as.integer(strsplit(itnmax_nTrees_rep,split="_")[[1]])
-  doSingleRun(itnmax=x[1],nTrees=x[2],rep=x[3])
+  doSingleRun(itnmax=x[1],nTrees=x[2],nTreesObserved=x[3],rep=x[4])
 }
-itnmax_nTrees_repVector<-c(outer(c(outer(paste(itnmaxVector,"_",sep=""),nTreesVector,"paste",sep="")),sequence(nrep),"paste",sep="_"))
+itnmax_nTrees_nTreesObservedVector<-c(outer(c(outer(paste(itnmaxVector,"_",sep=""),nTreesVector,"paste",sep="")),nTreesObservedVector,"paste",sep="_"))
+itnmax_nTrees_nTreesObserved_repVector<-c(outer(itnmax_nTrees_nTreesObservedVector,sequence(nrep),"paste",sep="_"))
 
-mc.cores<-max(1,detectCores()-1)
-mclapply(itnmax_nTrees_repVector,FUN=singleParamDoSingleRun,mc.cores=mc.cores)
+mc.cores<-max(1,detectCores()-2)
+mclapply(itnmax_nTrees_nTreesObserved_repVector,FUN=singleParamDoSingleRun,mc.cores=mc.cores)
