f0368c8a980711e61dc369d91cc017d198418f22
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-09 21:52:19 +0000

    data for speeding up runs

---
+++ pkg/data/BatchMakeFullyResolveCollapse.R
@@ -0,0 +1,10 @@
+source("../R/batchMS.R")
+for (npop in 1:5) {
+	for(maxK in 1:10) {
+		print(paste("Now starting ",npop," populations and maxK=",maxK))
+		start=proc.time()
+		try(generateMigrationIndividualsFullyResolvedCollapseAllowNoMigration(popVector=rep(5,npop),maxK=maxK,verbose=TRUE,file=paste("migrationArray_FullyResolvedCollapse_npop",npop,"_maxK",maxK,".Rsave",sep="")))	
+		print("That took")
+		print(proc.time()-start)
+	}
+}
