6d2ff6be4cf2972025c7001c06c70701dc3b2a6f
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-04 21:48:19 +0000

    added grid search

--- pkg/R/phrapl-search.R
+++ pkg/R/phrapl-search.R
@@ -21,38 +21,38 @@
   # }
 # }
 
-SearchContinuousModelSpaceOptim<-function(p, migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, maxParameterValue=100, nTrees=1 ,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=FALSE, debug=FALSE,method="nlminb",itnmax=NULL, return.all=FALSE, parameterBounds=list(minCollapseTime=0.1, minCollapseRatio=0, minN0Ratio=0.1, minMigrationRate=0.05, minMigrationRatio=0.1), ...) {
-  modelID<-ReturnModel(p,migrationArrayMap)
-  if(print.results) {
-    resultVector<-c(modelID,p)
-    names(resultVector)<-c("migrationArryIndividualID","collapseMatrix.number", "n0multiplierMap.number","migrationArray.number")
-    print(resultVector)
-  }
-  if(is.na(modelID)) {
-    return(badAIC)
-  }
-  else {
-    paramNames<-MsIndividualParameters(migrationArray[[modelID]])
-    startingVals<-log(c(rlnorm(sum(grepl("collapse",paramNames)),1,1), rlnorm(-1 + sum(grepl("n0multiplier",paramNames)),1,1), rbeta(sum(grepl("migration",paramNames)),shape1=1,shape2=3) ))  #going to optimize in log space. For N0, force first one to always be 1, so drop a parameter here
-    if(debug) {
-      print(startingVals) 
-    }
-    searchResults<-optim(par=startingVals, fn=ReturnAIC, method=method, control=list(maxit=itnmax), migrationIndividual=migrationArray[[modelID]], popVector=popVector, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, print.ms.string=print.ms.string, print.results=print.results, debug=debug, parameterBounds=parameterBounds, ...)
-
-  			#stitch the first N0multiplier (=1) into the final parameter vector
-			positionOfFirstN0 <- min(grep("n0multiplier", MsIndividualParameters(migrationArray[[modelID]])))
-			solutionVectorFirstPart<-searchResults$solution[sequence(positionOfFirstN0-1)]
-			solutionVectorSecondPart<-searchResults$solution[(1+length(solutionVectorFirstPart)):length(searchResults$solution)]
-			if((1+length(solutionVectorFirstPart)) > length(searchResults$solution)) {
-  				solutionVectorSecondPart<-c()
-			}
-    ifelse(return.all, return(searchResults), return(searchResults$value))     
-  }
-}
+# SearchContinuousModelSpaceOptim<-function(p, migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, maxParameterValue=100, nTrees=1 ,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=FALSE, debug=FALSE,method="nlminb",itnmax=NULL, return.all=FALSE, parameterBounds=list(minCollapseTime=0.1, minCollapseRatio=0, minN0Ratio=0.1, minMigrationRate=0.05, minMigrationRatio=0.1), ...) {
+#   modelID<-ReturnModel(p,migrationArrayMap)
+#   if(print.results) {
+#     resultVector<-c(modelID,p)
+#     names(resultVector)<-c("migrationArryIndividualID","collapseMatrix.number", "n0multiplierMap.number","migrationArray.number")
+#     print(resultVector)
+#   }
+#   if(is.na(modelID)) {
+#     return(badAIC)
+#   }
+#   else {
+#     paramNames<-MsIndividualParameters(migrationArray[[modelID]])
+#     startingVals<-log(c(rlnorm(sum(grepl("collapse",paramNames)),1,1), rlnorm(-1 + sum(grepl("n0multiplier",paramNames)),1,1), rbeta(sum(grepl("migration",paramNames)),shape1=1,shape2=3) ))  #going to optimize in log space. For N0, force first one to always be 1, so drop a parameter here
+#     if(debug) {
+#       print(startingVals) 
+#     }
+#     searchResults<-optim(par=startingVals, fn=ReturnAIC, method=method, control=list(maxit=itnmax), migrationIndividual=migrationArray[[modelID]], popVector=popVector, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, print.ms.string=print.ms.string, print.results=print.results, debug=debug, parameterBounds=parameterBounds, ...)
+# 
+#   			#stitch the first N0multiplier (=1) into the final parameter vector
+# 			positionOfFirstN0 <- min(grep("n0multiplier", MsIndividualParameters(migrationArray[[modelID]])))
+# 			solutionVectorFirstPart<-searchResults$solution[sequence(positionOfFirstN0-1)]
+# 			solutionVectorSecondPart<-searchResults$solution[(1+length(solutionVectorFirstPart)):length(searchResults$solution)]
+# 			if((1+length(solutionVectorFirstPart)) > length(searchResults$solution)) {
+#   				solutionVectorSecondPart<-c()
+# 			}
+#     ifelse(return.all, return(searchResults), return(searchResults$value))     
+#   }
+# }
 
 
 #TO DO: Add a starting grid (expand.grid()). Calculate AIC at each point, start at the Nstart best ones. If the optimal value is outside the bounds of the grid, offer warning or option to restart search centered at new grid
-SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, maxParameterValue=100, nTrees=1 ,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=FALSE, debug=FALSE,method="nlminb",itnmax=NULL, return.all=FALSE, maxtime=0, maxeval=0, parameterBounds=list(minCollapseTime=0.1, minCollapseRatio=0, minN0Ratio=0.1, minMigrationRate=0.05, minMigrationRatio=0.1), numReps=5, ...) {
+SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, maxParameterValue=100, nTrees=1 ,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=FALSE, debug=FALSE,method="nlminb",itnmax=NULL, return.all=FALSE, maxtime=0, maxeval=0, parameterBounds=list(minCollapseTime=0.1, minCollapseRatio=0, minN0Ratio=0.1, minMigrationRate=0.05, minMigrationRatio=0.1), numReps=5, startGrid=NULL, collapseStarts=c(0.1, 0.5, 2, 4, 8), n0Starts=c(0.1, 0.5, 1, 2, 4), migrationStarts=c(0.05, 0.1, 1), ...) {
   modelID<-ReturnModel(p,migrationArrayMap)
   best.result <- c()
   best.result.objective <- badAIC
@@ -66,23 +66,50 @@ SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray,
   }
   else {
     paramNames<-MsIndividualParameters(migrationArray[[modelID]])
+    if(is.null(startGrid)) { #need to make our own grid
+    	startingVectorList<-list()
+    	numCollapse <- sum(grepl("collapse",paramNames))
+    	if (numCollapse > 0) {
+    		for (i in sequence(numCollapse)) {
+    			startingVectorList<-append(startingVectorList, list(collapseStarts))
+    		} 
+    	}
+        numn0 <- sum(grepl("n0multiplier",paramNames))-1 #-1 since one is fixed to be 1
+    	if (numn0 > 0) {
+    		for (i in sequence(numn0)) {
+    			startingVectorList<-append(startingVectorList, list(n0Starts))
+    		} 
+    	}	
+    	numMigration <- sum(grepl("migration",paramNames))
+    	if (numMigration > 0) {
+    		for (i in sequence(numMigration)) {
+    			startingVectorList<-append(startingVectorList, list(migrationStarts))
+    		} 
+    	}
+    	startGrid <- log(expand.grid(startingVectorList)) #since optimize in log space
+    }
+    initial.AIC <- sapply(startGrid, 1, ReturnAIC, migrationIndividual=migrationArray[[modelID]], popVector=popVector, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, print.ms.string=print.ms.string, print.results=print.results, debug=debug, parameterBounds=parameterBounds)
+    if(debug) {
+    	print(cbind(initial.AIC, exp(startGrid)))
+    }
     for(rep in sequence(numReps)) {
- 	   startingVals<-log(c(rlnorm(sum(grepl("collapse",paramNames)),1,1), rlnorm(-1+sum(grepl("n0multiplier",paramNames)),1,1), rbeta(sum(grepl("migration",paramNames)),shape1=1,shape2=3) )) #going to optimize in log space. Remove the first n0 parameter from optimization vector
+ 	   #startingVals<-log(c(rlnorm(sum(grepl("collapse",paramNames)),1,1), rlnorm(-1+sum(grepl("n0multiplier",paramNames)),1,1), rbeta(sum(grepl("migration",paramNames)),shape1=1,shape2=3) )) #going to optimize in log space. Remove the first n0 parameter from optimization vector
+ 	   startingVals <- startGrid[order(initial.AIC)[rep],] #order(initial.AIC) gives indices of best values, min to max, so if we want the second best it's the second one here. NA are stuck last, if present
  	   if(debug) {
  	     print(startingVals) 
-    }
+    	}
  	   searchResults<-nloptr(x0=startingVals, eval_f=ReturnAIC, opts=list("maxeval"=itnmax, "algorithm"="NLOPT_LN_SBPLX", "print_level"=1, maxtime=maxtime, maxeval=maxeval), migrationIndividual=migrationArray[[modelID]], popVector=popVector, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, print.ms.string=print.ms.string, print.results=print.results, debug=debug, parameterBounds=parameterBounds)
- 	   if(debug) {
+ 	  
 
-  			#stitch the first N0multiplier (=1) into the final parameter vector
-			positionOfFirstN0 <- min(grep("n0multiplier", MsIndividualParameters(migrationArray[[modelID]])))
-			solutionVectorFirstPart<-searchResults$solution[sequence(positionOfFirstN0-1)]
-			solutionVectorSecondPart<-searchResults$solution[(1+length(solutionVectorFirstPart)):length(searchResults$solution)]
-			if((1+length(solutionVectorFirstPart)) > length(searchResults$solution)) {
+  		#stitch the first N0multiplier (=1) into the final parameter vector
+		positionOfFirstN0 <- min(grep("n0multiplier", MsIndividualParameters(migrationArray[[modelID]])))
+		solutionVectorFirstPart<-searchResults$solution[sequence(positionOfFirstN0-1)]
+		solutionVectorSecondPart<-searchResults$solution[(1+length(solutionVectorFirstPart)):length(searchResults$solution)]
+		if((1+length(solutionVectorFirstPart)) > length(searchResults$solution)) {
   			solutionVectorSecondPart<-c()
   		}
 		searchResults$solution<-c(solutionVectorFirstPart,exp(1),solutionVectorSecondPart)
-
+		 if(debug) {
  	   	print(searchResults)
  	   	}
  	   	if(searchResults$objective <= best.result.objective) {
