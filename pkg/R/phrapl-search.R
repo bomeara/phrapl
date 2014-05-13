758dbd874e9ffdf7279ffcabd489bc73a7bb6af9
ndjaxon <ndjaxon@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-13 20:13:06 +0000

    A few random tweaks

--- pkg/R/phrapl-search.R
+++ pkg/R/phrapl-search.R
@@ -55,7 +55,7 @@
 #I have tweaked the code for SearchContinuousSpaceNLopter such that 1) different grid parameters were used and such that 2)the grid file is saved
 #with a different name for each run
 SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray, popAssignments, badAIC=100000000000000, 
-	maxParameterValue=100, nTrees=2e5, nTreesGrid=nTrees ,msDir="/usr/local/bin/ms",compareDir="comparecladespipe.pl",
+	maxParameterValue=100, nTrees=2e5, nTreesGrid=nTrees ,msPath="ms",comparePath="comparecladespipe.pl",
 	unresolvedTest=TRUE,print.ms.string=FALSE, ncores=1,print.results=FALSE,print.matches=FALSE,debug=FALSE,method="nlminb",
 	itnmax=NULL, return.all=FALSE, maxtime=0, maxeval=0, parameterBounds=list(minCollapseTime=0.1, minCollapseRatio=0, 
 	minN0Ratio=0.1, minMigrationRate=0.05, minMigrationRatio=0.1), numReps=0, startGrid=startGrid, 
@@ -114,7 +114,7 @@ SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray,
   	initial.AIC<-data.frame()
   	for(i in 1:nrow(startGrid)){
     	currentAIC<-ReturnAIC(par=as.numeric(startGrid[i,]),migrationIndividual=migrationArray[[modelID]],
-    	badAIC=badAIC,maxParameterValue=maxParameterValue,nTrees=nTreesGrid,msDir=msDir,compareDir=compareDir,
+    	badAIC=badAIC,maxParameterValue=maxParameterValue,nTrees=nTreesGrid,msPath=msPath,comparePath=comparePath,
     	unresolvedTest=unresolvedTest,print.ms.string=print.ms.string,print.results=print.results,print.matches=print.matches,
     	ncores=ncores,debug=debug,parameterBounds=parameterBounds,subsamplesPerGene=subsamplesPerGene,
     	totalPopVector=totalPopVector,popAssignments=popAssignments,subsampleWeights=subsampleWeights,
@@ -151,7 +151,7 @@ SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray,
  	   	searchResults<-nloptr(x0=startingVals, eval_f=ReturnAIC, opts=list("maxeval"=itnmax, "algorithm"="NLOPT_LN_SBPLX", "print_level"=1,
  	   		maxtime=maxtime, maxeval=maxeval), migrationIndividual=migrationArray[[modelID]], popVector=popVector, 
  	   		badAIC=badAIC, maxParameterValue=maxParameterValue,
- 	   		nTrees=nTrees,msDir=msDir,compareDir=compareDir,unresolvedTest=unresolvedTest,ncores=ncores,
+ 	   		nTrees=nTrees,msPath=msPath,comparePath=comparePath,unresolvedTest=unresolvedTest,ncores=ncores,
  	   		print.ms.string=print.ms.string, print.results=print.results,print.matches=print.matches,
  	   		debug=debug, parameterBounds=parameterBounds,subsamplesPerGene=subsamplesPerGene,summaryFn=summaryFn,
  	   		totalPopVector=totalPopVector,subsampleWeights=subsampleWeights,popAssignments=popAssignments,
@@ -182,8 +182,8 @@ SearchContinuousModelSpaceNLoptr<-function(p, migrationArrayMap, migrationArray,
 #This function was formally known as "ExhaustiveSearchNLoptr", and still has the capabilities of running a heuristic
 #nloptr search. However, since we are currently focusing on a grid search, we have changed the name of the function
 GridSearch<-function(migrationArrayMap, migrationArray, popAssignments, badAIC=100000000000000, 
-		maxParameterValue=100,nTrees=1 ,msDir="/usr/local/bin/ms",compareDir="comparecladespip.pl",
-		observedDir="observed.tre",subsampleWeightsDir="subsampleWeights.txt",unresolvedTest=TRUE,
+		maxParameterValue=100,nTrees=1 ,msPath="ms",comparePath="comparecladespipe.pl",
+		observedPath="observed.tre",subsampleWeightsPath="subsampleWeights.txt",unresolvedTest=TRUE,
 		print.ms.string=FALSE,print.results=FALSE,print.matches=FALSE,debug=FALSE,method="nlminb",itnmax=NULL,
 		ncores=1,results.file=NULL,maxtime=0, maxeval=0,return.all=TRUE, numReps=0,startGrid=NULL,
 		collapseStarts=c(0.30,0.58,1.11,2.12,4.07,7.81,15.00), n0Starts=c(0.1,0.5,1,2,4), 
@@ -191,8 +191,8 @@ GridSearch<-function(migrationArrayMap, migrationArray, popAssignments, badAIC=1
 		subsampleWeights=NULL,summaryFn,saveNoExtrap=FALSE,...){
 
   #Prepare temporary tree and assign files
-	observed<-read.tree(observedDir)
-	subsampleWeights<-read.table(subsampleWeightsDir)
+	observed<-read.tree(observedPath)
+	subsampleWeights<-read.table(subsampleWeightsPath)
 	firstTree<-1
 	lastTree<-length(observed) / length(popAssignments)
 	for(k in 1:length(popAssignments)){
@@ -223,7 +223,7 @@ GridSearch<-function(migrationArrayMap, migrationArray, popAssignments, badAIC=1
  		}
   		result.indiv<-NULL
   		try(result.indiv<-SearchContinuousModelSpaceNLoptr(p,migrationArrayMap,migrationArray,popAssignments,badAIC=badAIC,
-  		maxParameterValue=maxParameterValue,nTrees=nTrees,msDir=msDir,compareDir=compareDir,unresolvedTest=unresolvedTest,
+  		maxParameterValue=maxParameterValue,nTrees=nTrees,msPath=msPath,comparePath=comparePath,unresolvedTest=unresolvedTest,
   		print.ms.string=print.ms.string,print.results=print.results,print.matches=print.matches,debug=debug,
   		method=method,itnmax=itnmax, maxtime=maxtime,ncores=ncores,maxeval=maxeval, return.all=return.all,numReps=numReps,
   		startGrid=currentStartGrid,gridSave=NULL,collapseStarts=collapseStarts,n0Starts=n0Starts,migrationStarts=migrationStarts,
@@ -237,7 +237,7 @@ GridSearch<-function(migrationArrayMap, migrationArray, popAssignments, badAIC=1
   		results.list<-append(results.list, list(result.indiv[[1]]))
   	} else {
   		try(AIC.values[i]<-SearchContinuousModelSpaceNLoptr(p,migrationArrayMap,migrationArray,popAssignments,badAIC=badAIC,
-  		maxParameterValue=maxParameterValue, nTrees=nTrees, msDir=msDir,compareDir=compareDir,ncores=ncores,
+  		maxParameterValue=maxParameterValue, nTrees=nTrees, msPath=msPath,comparePath=comparePath,ncores=ncores,
   		unresolvedTest=unresolvedTest,print.ms.string=print.ms.string,print.results=print.results,print.matches=print.matches,
   		debug=debug,method=method,itnmax=itnmax, maxtime=maxtime, maxeval=maxeval, return.all=return.all,numReps=numReps,
   		startGrid=currentStartGrid,collapseStarts=collapseStarts,n0Starts=n0Starts,migrationStarts=migrationStarts,
