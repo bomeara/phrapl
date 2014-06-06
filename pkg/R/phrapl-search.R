637f50adfe9239ac517736aa2a2641843ce9e824
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-06 21:29:37 +0000

    fixes for help files

--- pkg/R/phrapl-search.R
+++ pkg/R/phrapl-search.R
@@ -205,7 +205,7 @@ GridSearch<-function(modelRange=c(1:length(migrationArray)), migrationArrayMap,m
   
   	#Toss temporary tree and assign files
 	for(k in 1:length(popAssignments)){
-		unlink(c(paste("assign",k,".txt",sep=""),paste("observed",k,".tre",sep=""),"mstrees.txt"))
+		unlink(c(paste(tempdir(),"/assign",k,".txt",sep=""),paste(tempdir(),"/observed",k,".tre",sep=""),paste(tempdir(), "/mstrees.txt", sep="")))
 	}
 
 	#Save table of best models
@@ -236,45 +236,3 @@ GridSearch<-function(modelRange=c(1:length(migrationArray)), migrationArrayMap,m
 	ifelse(return.all, return(results.final), return(AIC.values))     
 }
 
-# searchDiscreteModelSpace<-function(migrationArrayMap, migrationArray, popVector, print.ms.string=FALSE, badAIC=100000000000000, maxParameterValue=100, nTrees=1,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, debug=FALSE,  method="newuoa", itnmax=NULL,pop.size=50, print.results=FALSE, ...) {
-  # Domains<-matrix(ncol=2,nrow=3)
-  # Domains[1,]<-range(migrationArrayMap$collapseMatrix.number)
-  # Domains[2,]<-range(migrationArrayMap$n0multiplierMap.number)
-  # Domains[3,]<-range(migrationArrayMap$migrationArray.number)
-  
-  # results<-genoud(searchContinuousModelSpace,nvars=3, max=FALSE,starting.values=c(1,1,1), MemoryMatrix=TRUE, boundary.enforcement=2, data.type.int=TRUE, Domains=Domains, migrationArrayMap=migrationArrayMap, migrationArray=migrationArray, popVector=popVector, print.ms.string=print.ms.string, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, debug=debug, method=method,itnmax=itnmax, pop.size=pop.size, print.results=print.results,...)
-  # return(results)
-# }
-
-# SearchDiscreteModelSpaceOptim<-function(migrationArrayMap, migrationArray, popVector, print.ms.string=FALSE, badAIC=100000000000000, maxParameterValue=100, nTrees=1,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, debug=FALSE,  method="BFGS", itnmax=NULL,pop.size=50, print.results=FALSE, ...) {
-  # Domains<-matrix(ncol=2,nrow=3)
-  # Domains[1,]<-range(migrationArrayMap$collapseMatrix.number)
-  # Domains[2,]<-range(migrationArrayMap$n0multiplierMap.number)
-  # Domains[3,]<-range(migrationArrayMap$migrationArray.number)
-  
-  # results<-genoud(SearchContinuousModelSpaceOptim,nvars=3, max=FALSE,starting.values=c(1,1,1), MemoryMatrix=TRUE, boundary.enforcement=2, data.type.int=TRUE, Domains=Domains, migrationArrayMap=migrationArrayMap, migrationArray=migrationArray, popVector=popVector, print.ms.string=print.ms.string, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, debug=debug, method=method,itnmax=itnmax, pop.size=pop.size, print.results=print.results,...)
-  # return(results)
-# }
-
-# SearchDiscreteModelSpaceNLoptr<-function(migrationArrayMap, migrationArray, popVector, print.ms.string=FALSE, badAIC=100000000000000, maxParameterValue=100, nTrees=1,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, debug=FALSE,  method="nlminb", itnmax=NULL,pop.size=50, print.results=FALSE, maxtime=0, maxeval=0, ...) {
-  # Domains<-matrix(ncol=2,nrow=3)
-  # Domains[1,]<-range(migrationArrayMap$collapseMatrix.number)
-  # Domains[2,]<-range(migrationArrayMap$n0multiplierMap.number)
-  # Domains[3,]<-range(migrationArrayMap$migrationArray.number)
-  
-  # results<-genoud(SearchContinuousModelSpaceNLoptr,nvars=3, max=FALSE,starting.values=c(1,1,1), MemoryMatrix=TRUE, boundary.enforcement=2, data.type.int=TRUE, Domains=Domains, migrationArrayMap=migrationArrayMap, migrationArray=migrationArray, popVector=popVector, print.ms.string=print.ms.string, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees,msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, debug=debug, method=method,itnmax=itnmax, pop.size=pop.size, print.results=print.results, maxtime=maxtime, maxeval=maxeval, return.all=FALSE,...)
-  # return(results)
-# }
-
-# ExhaustiveSearchOptim<-function(migrationArrayMap, migrationArray, popVector, badAIC=100000000000000, maxParameterValue=100, nTrees=1 ,msLocation="/usr/local/bin/ms",compareLocation="comparecladespipe.pl",assign="assign.txt",observed="observed.txt",unresolvedTest=TRUE, print.ms.string=FALSE, print.results=FALSE, debug=FALSE,method="nlminb",itnmax=NULL, ncores=1, results.file=NULL, return.all=TRUE, ...) {
-  # AIC.values<-rep(NA,length(migrationArray))
-  # for (i in sequence(length(migrationArray))) {
-  	# p<-c(migrationArrayMap$collapseMatrix.number[i], migrationArrayMap$n0multiplierMap.number[i], migrationArrayMap$migrationArray.number[i])
-  	# try(AIC.values[i]<-SearchContinuousModelSpaceOptim(p, migrationArrayMap, migrationArray, popVector, badAIC=badAIC, maxParameterValue=maxParameterValue, nTrees=nTrees, msLocation=msLocation,compareLocation=compareLocation,assign=assign,observed=observed,unresolvedTest=unresolvedTest, print.ms.string=print.ms.string, print.results=print.results, debug=debug,method=method,itnmax=itnmax,ncores=ncores,results.file=results.file,return.all=return.all,...))
-  	# print(c(i, length(migrationArray), i/length(migrationArray), AIC.values[i]))
-	# if(!is.null(results.file)) {
-		# save(list=ls(), file=results.file)
-	# }
-  # }
-  # return(AIC.values)
-# }
